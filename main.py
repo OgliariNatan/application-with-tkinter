497d977dcf83cf1e2bd5e8c42cea1ba26af1acd0
OgliariNatan <natanogliari@gmail.com>
2024-08-13 10:27:38 -0300

    tkinter

--- main.py
+++ main.py
@@ -3,16 +3,17 @@
 from tkinter import *
 def bt_click():
     print("BT Click")
+    lb["text"] = 'ESCREVEU EM CIMA'
 
 
 
 
 janela = Tk()
 
-bt = Button(janela, width=20, text="OK", command=bt_click) #Cria o botão diz a quem pertence, ajusta o tamanho e o texto
+bt = Button(janela, width=10, text="OK", command=bt_click) #Cria o botão diz a quem pertence, ajusta o tamanho e o texto
 bt.place(x=100, y=100) #Ajusta a posição do botão na tela
 
-lb = Label(janela, text='Informações', width=10)
+lb = Label(janela, text='Informações', width=20)
 lb.place(x=100, y=150)
 
 
