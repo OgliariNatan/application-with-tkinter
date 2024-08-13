5d5afe6892812cca81a2e1d13dedf62b823faebd
OgliariNatan <natanogliari@gmail.com>
2024-08-13 10:26:08 -0300

    init tkinter

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from tkinter import *
+def bt_click():
+    print("BT Click")
+
+
+
 
 janela = Tk()
-print('kj')
+
+bt = Button(janela, width=20, text="OK", command=bt_click) #Cria o botão diz a quem pertence, ajusta o tamanho e o texto
+bt.place(x=100, y=100) #Ajusta a posição do botão na tela
+
+lb = Label(janela, text='Informações', width=10)
+lb.place(x=100, y=150)
 
 
 janela.geometry("300x300+200+200")
